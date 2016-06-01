853dead3833b8b7496e6b3f1204d2df80aded650
Ryan Speed <rspeed@speedo.ca>
2016-06-01 14:07:18 -0700

    move file resources to their own manifest

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,47 +1,7 @@
 # Class: play
 # ===========================
 #
-# Full description of class play here.
-#
-# Parameters
-# ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'play':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
-# Authors
-# -------
-#
-# Author Name <author@domain.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2016 Your name here, unless otherwise noted.
-#
+
 class play (
   $user     = $::play::params::user,
   $group    = $::play::params::group,
@@ -61,34 +21,5 @@ class play (
     gid    => $group,
     home   => $homepath,
   }
-  ->
-  file { 'playappdir':
-    ensure => 'directory',
-    path   => $homepath,
-    owner  => $user,
-    group  => $group,
-    mode   => '0775',
-  }
-  file { 'playlogs':
-    ensure  => 'directory',
-    path    => "${homepath}/logs",
-    owner   => $user,
-    group   => $group,
-    require => File['playappdir'],
-    mode    => '0775',
-  }
-  file { 'playpids':
-    ensure  => 'directory',
-    path    => "${homepath}/pids",
-    owner   => $user,
-    group   => $group,
-    require => File['playappdir'],
-    mode    => '0775',
-  }
-  file { [ "${homepath}/cache", "${homepath}/cache/zip"] :
-    ensure => 'directory',
-    owner  => $user,
-    group  => $group,
-    mode   => '0755',
-  }
+
 }
