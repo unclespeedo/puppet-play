1539b5e46b20a30d34974c2489d18a617f16a17c
Ryan Speed <rspeed@speedo.ca>
2016-07-04 13:20:11 -0700

    add repo management and fix service management

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,13 @@
 class play::params {
   $config_params     =  hiera_hash('play::config_params', undef)
   $package_manage    = false
-  $repo_trusted      = true
+  $repo_manage       = false
+  $repo_trusted      = false
   $package_ensure    = 'latest'
   $package_name      = 'play'
+  $service_manage    = false
   $service_enable    = true
   $service_ensure    = 'running'
-  $service_manage    = true
   $service_name      = 'play'
   $user              = 'play'
   $group             = 'play'
